065e7735f3cf8b640bf40231c238578332b5cc22
mrdeepak8461-pro <mr.deepakgit8461@gmail.com>
2021-02-25 15:24:42 +0530

    Ravi photography

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.materialboxed').materialbox();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots:false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
